43545fb5d832f8589f70a84b82b7cb8952457a7f
rtenhaus <joe.gartenhaus@gmail.com>
2022-05-25 14:36:19 -0700

    staging model - recharge subscriptions & documentation

--- models/staging/recharge/stg_recharge_subscriptions.sql
+++ models/staging/recharge/stg_recharge_subscriptions.sql
@@ -17,14 +17,18 @@ with source as (
         , safe_cast(customer_id as int64) as customer_id
         
         -- Timestamps
-        , safe_cast(created_at as timestamp) as created_at_et
+        , safe_cast(created_at as timestamp) as created_at
         , safe_cast(created_at as date) as created_date
-        , safe_cast(cancelled_at as timestamp) as cancelled_at_et
-        , safe_cast(cancelled_at as date) as cancelled_date
+        , if(status = 'CANCELLED',coalesce(safe_cast(cancelled_at as timestamp), safe_cast(updated_at as timestamp)), safe_cast(cancelled_at as timestamp)) as cancelled_at
+        , if(status = 'CANCELLED',coalesce(safe_cast(cancelled_at as date), safe_cast(updated_at as date)), safe_cast(cancelled_at as date)) as cancelled_date
 
         -- Needed Fields
         , safe_cast(status as string) as status
         , safe_cast(cancellation_reason as string) as cancellation_reason
+        , status = 'CANCELLED' 
+            and (lower(cancellation_reason) not like '%max number of charge attempts%' 
+                    or cancellation_reason is null) as is_valid_cancellation
+        , row_number() over(partition by customer_id order by created_at asc) as customer_subscription_number
 
         -- No need to stage the rest of the data for this exercise, include the un-staged data anyway
         , * except(id, customer_id, created_at, cancelled_at, status, cancellation_reason)
